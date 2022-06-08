7483314e90fa49b05ec20038fade551cfaffd342
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-06-08 11:26:57 -0700

    Added variables

--- script.js
+++ script.js
@@ -12,3 +12,48 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// variables
+
+var upperCaseLetters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var lowerCaseLetters = [  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+var specialCharacters = [ "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~" ];
+
+var length;
+
+var useUpper = true;
+var useLower = true;
+var useNumbers = true;
+var useSpecial = true;
+
+// prompt for password length
+// assign to length
+
+// validate length => 8 && length <= 128
+
+// prompt lowercase
+// assign to lower
+
+// prompt upper case 
+// assign to upper
+
+// prompt numbers
+// assign to numbers
+
+// prompt special characters
+// assign to special
+
+// validate at least one character type is selected
+
+
+// add generate password function
+function generatePassword () {
+  
+  // local variables
+  var generatedPassword = "";
+
+
+  return;
+}
